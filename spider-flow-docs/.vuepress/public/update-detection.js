615a6c2dbc22592e5addb90c86e90e60cbe25f41
mxd <838425805@qq.com>
2020-04-02 10:16:40 +0800

    update

--- spider-flow-docs/.vuepress/public/update-detection.js
+++ spider-flow-docs/.vuepress/public/update-detection.js
@@ -2,7 +2,7 @@ if("undefined" != typeof SPIDER_FLOW_VERSION){
     if(SPIDER_FLOW_VERSION < '0.4.3'){
         layui.layer.open({
             title : '版本更新',
-            content : '新版本v0.4.2已发布',
+            content : '新版本v0.4.3已发布',
             btn : ['前去看看','忽略'],
             yes : function(index){
                 layui.layer.close(index);
